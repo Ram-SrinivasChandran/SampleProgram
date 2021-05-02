e01d285ad993e9acb6787fc54d36430461b6c8d9
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2021-05-02 00:15:10 -0400

    Add files via upload

---
+++ Primenumber.java
@@ -0,0 +1,15 @@
+public class Primenumber{
+	public static void main(String[] args) {
+		int n=3;
+		if(n%2==0)
+		{
+			System.out.println(n+"is not a prime number");
+		}if(n==1||n==0)
+		{
+			System.out.println(n+"is not a prime number");
+		}else
+		{
+			System.out.println(n+"is a prime number");
+		}
+	}
+	}
