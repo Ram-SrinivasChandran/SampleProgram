1a1ac84041eabdeaeeddde1ee450914d90813504
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2021-05-02 08:37:40 -0400

    Add files via upload

---
+++ Randomnumber.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+public class Randomnumber 
+{
+	public static void main(String[] args) 
+	{
+
+	System.out.println("1 st Randomnumber "+ Math.random());
+	System.out.println("2 nd Randomnumber "+ Math.random());
+	System.out.println("3 rd Randomnumber "+ Math.random());
+	System.out.println("4 th Randomnumber "+ Math.random());	
+	}
+}
