5f902fa3c36c7ff6cdfd7177ce18ba816658ea2f
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2021-05-04 07:11:30 -0400

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Multiply{
+	public static void main(String[] args) {
+		float a,b,c;
+		Scanner in=new Scanner(System.in);
+		Scanner out=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		a=in.nextFloat();
+		System.out.println("Enter a number:");
+		b=out.nextFloat();
+		c=a*b;
+		System.out.println("The Product is "+c);
+	}
+}
