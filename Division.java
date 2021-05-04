7480af1d3d28a49c78341da2353b0e79ad2c7ce9
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2021-05-04 07:48:57 -0400

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,11 @@
+public class Division{
+	public static void main(String[] args) {
+		int a=25,b=3,c,d;
+     		c=a/b;
+     		d=a%b;
+     		System.out.println("The Quotient is "+c);
+     		System.out.println("The Remainder is "+d);
+
+
+	}
+}
