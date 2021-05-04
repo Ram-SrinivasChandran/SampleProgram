d969ec48fb0a64562f6d85d6db77dd3935885b39
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2021-05-04 08:46:13 -0400

    Add files via upload

---
+++ Oddeven.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Oddeven{
+	public static void main(String[] args) {
+		Scanner in= new Scanner(System.in);
+		System.out.println("Enter a number");
+		int a=in.nextInt(); 
+	    if(a%2==0)  
+	    {
+	    		System.out.println("Even");
+
+	    	}	
+	    	else
+	    	{
+	    		System.out.println("odd");
+	    	}
+	}
+}
