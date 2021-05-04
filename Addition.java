5fb9a0a339f4fdfb56f7bd58096e07f26b4d7081
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2021-05-04 07:08:35 -0400

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Addition{
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner in=new Scanner(System.in);
+		Scanner out=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		a=in.nextInt();
+		System.out.println("Enter a number:");
+		b=out.nextInt();
+		c=a+b;
+		System.out.println("The answer is "+c);
+	}
+}
