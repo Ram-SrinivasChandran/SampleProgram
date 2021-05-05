e9cb961cb12ee8af380e55e720febc9d1abce7ef
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2021-05-04 23:45:05 -0400

    Add files via upload

---
+++ Lcm.java
@@ -0,0 +1,16 @@
+public class Lcm
+{
+	public static void main(String[] args) {
+		int a=72,b=120;
+		int gcd=1;
+		for(int i=1;i<=a&&i<=b;++i)
+		{
+			
+			if(a%i==0&&b%i==0)
+				gcd=i;
+
+		}
+		int lcm=(a*b)/gcd;
+		System.out.println("The Gcd of "+a+" and "+b+" is "+lcm);
+	}
+}
