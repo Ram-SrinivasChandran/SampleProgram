00b30ad346603d0f60368d8a1e93171bd29d4d22
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2021-05-06 03:43:19 -0400

    Add files via upload

---
+++ Perimetersquare.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Perimetersquare
+{
+	public static void main(String[] args) {
+		double a,perimeter;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the Radius:");
+		a=s.nextDouble();
+		perimeter=4*a;
+		System.out.println("The Perimeter of the Square is "+perimeter); 
+	}
+
+}
