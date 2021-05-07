e21229b841d885ef02c9c8e16d6d6c9c4e4882ae
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2021-05-06 22:25:11 -0400

    Add files via upload

---
+++ Perimeterrhombus.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Perimeterrhombus
+{
+	public static void main(String[] args) {
+		double a,perimeter;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the side of the Rhombus:");
+		a=s.nextDouble();
+		perimeter=4*a;
+		System.out.println("The Perimeter of the Rhombus is:"+perimeter);
+
+	}
+}
