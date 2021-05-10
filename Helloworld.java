548e65d37c694c92ed3e94817326483e50aa2b6b
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2021-05-09 23:03:27 -0400

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Helloworld
+{
+	public static void main(String[] args) {
+		String n;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a String ");
+		n = in.next();
+		System.out.println("The Entered String is "+n); 
+	}
+}
