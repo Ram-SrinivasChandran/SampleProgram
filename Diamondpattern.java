cc481f874be82c0d77b325e34ae8f0447954338a
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2021-05-15 08:52:21 +0530

    Add files via upload

---
+++ Diamondpattern.java
@@ -0,0 +1,47 @@
+import java.util.*;  
+class Diamondpattern  
+{  
+public static void main(String[] args)  
+{
+int rows,i,j,k;  
+Scanner sc = new Scanner(System.in);  
+System.out.println("Enter the number of rows you want to print: ");  
+rows = sc.nextInt();      
+for (i=1; i<= rows ; i++)   
+{  
+for (j = rows; j > i ; j--)   
+{  
+System.out.print(" ");  
+}  
+System.out.print("*");  
+for (k = 1; k < 2*(i -1) ;k++)   
+{   
+System.out.print(" ");   
+}  
+if( i==1)   
+{   
+System.out.println("");  
+}  
+else  
+{  
+System.out.println("*");   
+}  
+}   
+for (i=rows-1; i>= 1 ; i--)  
+{  
+for (j = rows; j > i ; j--)   
+{  
+System.out.print(" ");  
+}  
+System.out.print("*");  
+for (k = 1; k < 2*(i -1) ;k++)   
+{  
+System.out.print(" ");  
+}  
+if(i==1)  
+System.out.println("");  
+else  
+System.out.println("*");  
+}  
+}  
+}  
