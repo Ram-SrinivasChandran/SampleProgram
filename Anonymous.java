13f4df0f88e4c71a1b0a5bf2ffa8caf6a058bb19
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2021-06-03 09:20:05 +0530

    Add files via upload

---
+++ Anonymous.java
@@ -0,0 +1,14 @@
+class Anonymous
+{
+	static void printArray(int arr[])
+	{
+	for(int i=0;i<=arr.length;i++)
+	{
+		System.out.println(arr[i]);
+	}
+
+	}
+	public static void main(String[] args) {
+		printArray(new int[]{10,20,30,40,50});
+	}
+}
