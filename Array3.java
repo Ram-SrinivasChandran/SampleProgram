d37e0ce776956736d26b81e6cbacc586176feaf4
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2021-06-03 09:11:08 +0530

    Add files via upload

---
+++ Array3.java
@@ -0,0 +1,14 @@
+class Array3
+{
+	public static void main(String[] args) {
+		int a[]={10,20,30,40,50};
+		answer(a);
+	}
+	public static void answer(int arr[])
+	{
+		for(int i=0;i<=arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+}
