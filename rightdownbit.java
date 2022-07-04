dbde3b1e1833c526c845ce47fceb9dbd802f622a
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2022-07-04 23:10:15 +0530

    Create rightdownbit.java

---
+++ rightdownbit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Virtusa {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b=a & a-1;
+        System.out.println(b);
+
+    }
+}
