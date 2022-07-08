9ced0687b302b4a6bb93619b1a633d6648ca9cf4
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2022-07-08 12:26:38 +0530

    Create subtractwithoutminus1.java

---
+++ subtractwithoutminus1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Virtusa{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int sub=0;
+        for(int i=b;i<a;i++)
+        {
+            sub++;
+        }
+        System.out.println(sub);
+    }
+}
