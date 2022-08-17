0b00d21d0ed6558f22c40747036c9237b2b62d55
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2022-08-17 09:56:34 +0530

    Update sundaycount.java

--- sundaycount.java
+++ sundaycount.java
@@ -7,29 +7,29 @@ class Study
         int a=s.nextInt();
         int count=0,c=0;
         String [] b={"mon","tue","wed","thu","fri","sat","sun"};
-        for(int i=1;i<=a;i++)
-        {
-            for(int j=0;j<b.length;j++)
-            {
-                if(day=="sun")
-                {
-                    count++;
-                }
-                else
-                {
-                    c++;
-                    if(c==7)
-                    {
-                        j=0;
-                        a=a-7;
-                        i=0;
-                        c=0;
+        for(int i=1;i<=a;i++) {
+            if (a >= 7) {
+                for (int j = 0; j < b.length; j++) {
+                    if (day == b[6]) {
+                        count++;
+                    } else {
+                        c++;
+                        if (c == 7) {
+                            j = 0;
+                            a = a - 7;
+                            i = 0;
+                            c = 0;
+
+                        }
                     }
-                    if(b[j]=="sun")
-                    {
+                    if (b[j] == "sun") {
                         count++;
                     }
                 }
+            }
+            else
+            {
+                break;
             }
         }
         System.out.println(count);
